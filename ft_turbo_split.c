514e41ec171811dc412b286a24e130ec2a135168
gymnopediese <kality.of.life@gmail.com>
2023-07-02 21:45:51 +0200

    push

--- ft_turbo_split.c
+++ ft_turbo_split.c
@@ -6,7 +6,7 @@
 /*   By: albaud <albaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 12:12:31 by albaud            #+#    #+#             */
-/*   Updated: 2023/07/02 17:33:15 by albaud           ###   ########.fr       */
+/*   Updated: 2023/07/02 21:45:42 by albaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,28 @@ char	**ft_split(char *str, char c)
 {
 	char			*d;
 	char			**r;
-	const size_t	i[6] = (const size_t []){-1, -1, -1, 0, -1, -1};
+	const size_t	i[9] = (const size_t []){-1, -1, -1, 0, -1, -1, -1, -1, -1};
 
-	while (str && str[++((size_t *)i)[1]])
+	while (str && str[++((size_t *)i)[1]] && ((i[6] && ((str[i[1]] == c &&
+		++((size_t *)i)[7] < (size_t)-1) || ++((size_t *)i)[6])) || 1))
 		;
-	d = malloc(i[1] + (str != 0));
+	d = malloc((i[1] - (i[7] + 1) + (str != 0)));
 	if (d == 0)
 		return (0);
-	while (str[++((size_t *)i)[0]])
+	while (++((size_t *)i)[0] < i[1] - i[7])
 		d[i[0]] = 0;
-	while (str[++((size_t *)i)[2]])
-		if (str[i[2]] != c || (i[2] && d[i[2] - 1] && ++((size_t *)i)[3] > -1))
-			d[i[2]] = str[i[2]];
-	r = malloc((i[3] + 4 - (d[i[2]] == 0) - (d[i[1] - 1] == 0)) * 8);
+	while (++((size_t *)i)[8] < (size_t)-1 && str[++((size_t *)i)[7]])
+		if (str[i[7]] != c || (i[8] && d[i[8] - 1] && ++((size_t *)i)[3] > -1))
+			d[i[8]] = str[i[7]];
+	r = malloc(((i[3] + 2 - (d[i[8] - (i[8] != 0)] == 0)) * 8));
 	if (r == 0)
 		free(d);
 	if (r == 0)
 		return (0);
-	while ((long long)++((size_t *)i)[4] < (long long)(i[1] - 1))
+	while ((long long)++((size_t *)i)[4] < (long long)(i[8] - (i[8] != 0)))
 		if ((i[4] == 0 && d[i[4]] != 0) || (d[i[4] + 1] != 0 && d[i[4]] == 0))
 			r[++((size_t *)i)[5]] = &d[i[4] + !(i[4] == 0 && d[i[4]] != 0)];
 	r[++((size_t *)i)[5]] = 0;
-	r[++((size_t *)i)[5]] = d;
 	return (r);
 }
 
@@ -48,18 +48,18 @@ int	main(int argc, char **argv)
 	char	**r;
 	size_t	i;
 
-	ft_split(0, 0);
 	if (argc != 2)
 	{
 		printf("invalid args\n");
 		return (0);
 	}
 	r = ft_split(argv[1], ' ');
+	printf("mmais\n");
 	if (r == 0)
 		return (0);
 	if (r[0] == 0)
 	{
-		free(r[1]);
+		free(r[0]);
 		free(r);
 		printf("split[0] is null\n");
 		return (0);
@@ -67,6 +67,6 @@ int	main(int argc, char **argv)
 	i = -1;
 	while (r[++i])
 		printf("%s$\n", r[i]);
-	free(r[++i]);
+	free(r[0]);
 	free(r);
 }
